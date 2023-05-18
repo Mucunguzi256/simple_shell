d3c6c7f4b371b6a192b92e30bc9943df7c6bb043
mucunguzi256 <moemucu@gmail.com>
2023-05-18 16:43:27 -0700

    commiting a a betty respectful code for task 1

---
+++ sudst.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/*
+ * main entry point
+ * @a: int one
+ * @b: omt two
+ * @diff: the difference between a and b
+ * Return: The diff
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int diff = a - b;
+
+	printf("%d/n", diff);
+	return (0);
+}
